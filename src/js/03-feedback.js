19a3640b9a9fa7f71a589a3b103563a1c7a53ef5
PatrycjaNowakowska <poczta.pnowakowska@gmail.com>
2021-09-25 17:19:52 +0200

    task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector(".feedback-form");
+const formEmail = document.querySelector('[name="email"]')
+const formMessage = document.querySelector('[name="message"]');
+
+const LOCALSTORAGE_KEY = "feedback-form-state";
+
+const saveData = () => {
+    let data = {
+        email: `${formEmail.value}`,
+        message: `${formMessage.value}`,
+    };
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(data));
+};
+
+const updateInput = () => {
+    try {
+        formEmail.value = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)).email;
+        formMessage.value = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)).message;
+    } catch {
+        console.log("There is no data. Please enter data!");
+    }
+};
+
+const submit = e => {
+    e.preventDefault();
+    const {
+        elements: { email, message },
+    } = e.currentTarget;
+    console.log(JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)));
+    form.reset();
+    e.currentTarget.reset();
+    localStorage.clear();
+};
+
+updateInput();
+form.addEventListener('input', throttle(saveData, 500));
+form.addEventListener('submit', submit);
